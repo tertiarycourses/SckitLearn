954235116cd4a4e0602a62dc4844e0dc7d13d9d9
Tertiary Infotech <angch@tertiaryinfotech.com>
2016-12-25 10:19:22 +0800

    25 dec

---
+++ exercises/module5.py
@@ -0,0 +1,21 @@
+# Code guide for Python Scikit Learning Essential Training
+# Copyright: Tertiary Infotech Pte Ltd
+# Author: Dr Alfred Ang
+# Date: 27 Aug 2016
+
+# Module 5: Intro to Neural Networks
+
+# iris = datasets.load_iris()
+# X,y = iris.data, iris.target
+
+# from sklearn import neural_network
+# clf = neural_network.MLPClassifier(2,10,30)
+
+# clf.fit(X_train,y_train)
+
+# clf.predict(X_test)
+# print(y_test)
+
+
+
+
